6673bd012cffe08b60945fd754e4d3a0c7dc58b8
closertb <closertb@sina.com>
2020-11-02 00:05:41 +0800

    feat: 增加sticky

--- src/pages/Css/index.js
+++ src/pages/Css/index.js
@@ -11,6 +11,18 @@ const arr = new Array(35).fill(1).map((_, index) => ({
 
 const pageSize = 10;
 
+const styleMaps = {
+  0: 'js-fixed',
+  1: 'sticky',
+  2: 'viewport',
+};
+
+const nameMaps = {
+  0: 'js-fixed',
+  1: 'sticky',
+  2: 'viewport',
+};
+
 const mockFetch = (page) => {
   const end = page * pageSize > arr.length ? arr.length : page * pageSize;
   return new Promise((resolve) => {
@@ -23,6 +35,7 @@ const mockFetch = (page) => {
 export default function CssDemo() {
   const [list, setList] = useState([]);
   const [showDemo, setDemo] = useState(false);
+  const [sticky, setSticky] = useState(0);
   const pageRef = useRef(1);
 
   const [tags, setTags] = useState(0);
@@ -44,7 +57,6 @@ export default function CssDemo() {
         } else {
           setTags(0);
         }
-        console.log('data', pageRef.current, data);
         pageRef.current += 1;
         setList([...list, ...data]);
       });
@@ -52,25 +64,27 @@ export default function CssDemo() {
   }, [list, tags]);
 
   useEffect(() => {
-    // const demo = document.querySelector('#demo');
-    // const content = document.querySelector('#content');
-    // const titleHeight = document.querySelector('#title').clientHeight;
-    // let fixed = false;
-    // demo.addEventListener('scroll', (e) => {
-    //   if (!fixed && e.target.scrollTop >= titleHeight) {
-    //     fixed = true;
-    //     content.classList.add('with-fixed');
-    //   }
+    if (!sticky) {
+      const demo = document.querySelector('#demo');
+      const content = document.querySelector('#content');
+      const titleHeight = document.querySelector('#title').clientHeight;
+      let fixed = false;
+      demo.addEventListener('scroll', (e) => {
+        if (!fixed && e.target.scrollTop >= titleHeight) {
+          fixed = true;
+          content.classList.add('with-fixed');
+        }
 
-    //   if (fixed && e.target.scrollTop < titleHeight - 5) {
-    //     content.classList.remove('with-fixed');
-    //     fixed = false;
-    //   }
-    // });
+        if (fixed && e.target.scrollTop < titleHeight) {
+          content.classList.remove('with-fixed');
+          fixed = false;
+        }
+      });
+    }
     if (showDemo) {
       return;
     }
-    const list = document.querySelector('#list');
+    const list = document.querySelector(sticky < 2 ? '#demo' : '#list');
 
     const debunceListener = debounce((e) => {
       if (list.scrollHeight - list.clientHeight - e.target.scrollTop < 50) {
@@ -80,27 +94,36 @@ export default function CssDemo() {
     list.addEventListener('scroll', debunceListener);
 
     if (pageRef.current === 1) fetchRef.current(1);
-  }, [showDemo]);
+  }, [showDemo, sticky]);
 
   return (
-    <div id="demo" className={style.demo}>
-      <h3 id="title" className="title">
-        <div>这是一个概览头部：{showDemo ? '动态吸底' : '局部吸顶'}</div>
-        <button type="button" onClick={() => { setDemo(!showDemo); }}>点击切换demo</button>
-      </h3>
-      {!showDemo ?
-        <div id="content" className={`content ${(tags & 1) ? 'loading' : ''}`}>
-          <div className="filter-bar">
-            <h3>这是列表头部</h3>
-            <h3>可筛选</h3>
-            <h3>下面是滚动列表</h3>
+    <div className={style.demo}>
+      <div id="demo" className={`wrapper ${styleMaps[sticky]}  ${(tags & 1) ? 'loading' : ''}`}>
+        <h3 id="title" className="title">
+          <div>这是一个概览头部：{showDemo ? '动态吸底' : '局部吸顶'}</div>
+          <div>
+            <button type="button" onClick={() => { setDemo(!showDemo); }}>
+              点击切换为{showDemo ? '吸底' : '吸顶'}
+            </button>
+            <button type="button" onClick={() => { setSticky((sticky + 1) % 3); }} className="ml-20">
+              切换为{nameMaps[(sticky + 1) % 3] }
+            </button>
           </div>
-          <ul id="list" className="list">
-            {list.map(({ key, label }) => <li key={key}>{label}</li>)}
-            {tags > 1 && <div className="btm-tips">我也是有底线的</div>}
-          </ul>
-        </div> : <FlexFooter />
-      }
+        </h3>
+        {!showDemo ?
+          <div id="content" className="content">
+            <div className="filter-bar">
+              <h3>这是列表头部</h3>
+              <h3>可筛选</h3>
+              <h3>下面是滚动列表</h3>
+            </div>
+            <ul id="list" className="list">
+              {list.map(({ key, label }) => <li key={key}>{label}</li>)}
+              {tags > 1 && <div className="btm-tips">我也是有底线的</div>}
+            </ul>
+          </div> : <FlexFooter />
+        }
+      </div>
     </div>
   );
 }
